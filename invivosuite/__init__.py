6f2cc1931a10102bb982cbe139ceeb75084e7532
Lars Henrik Nelson <larshnelson@protonmail.com>
2023-11-28 17:04:03 -0500

    Updated wavelet

--- invivosuite/__init__.py
+++ invivosuite/__init__.py
@@ -1,3 +1,10 @@
-from .acq import AcqManager, lfp, spike_manager  # noqa: F401
+from .acq import (
+    AcqManager,  # noqa: F401
+    lfp,  # noqa: F401
+    spike_manager,  # noqa: F401
+    load_hdf5_acqs,  # noqa: F401
+    SpikeModel,  # noqa: F401
+    spike,  # noqa: F401
+)
 from . import utils  # noqa: F401
 from . import spectral  # noqa: F401
--- invivosuite/spectral/wavelet.py
+++ invivosuite/spectral/wavelet.py
@@ -1,15 +1,17 @@
 from typing import Literal
+import multiprocessing
 
 import numpy as np
 from numba import njit, prange
 from scipy import fft
+import pyfftw
 
 __all__ = ["create_wavelets", "compute_cwt"]
 
 
 @njit(cache=True, parallel=True)
 def create_wavelets(
-    freqs, fs, n_cycles=7, sigma=-1, zero_mean: bool = True, ord: Literal[1, 2] = 1
+    freqs, fs, n_cycles, sigma=-1, zero_mean: bool = True, order: Literal[1, 2] = 1
 ):
     """Compute Morlet wavelets for the given frequency range.
 
@@ -39,11 +41,6 @@ def create_wavelets(
         The wavelets time series. If ``freqs`` was a float, a single
         ndarray is returned instead of a list of ndarray.
     """
-    Ws = []
-    if not isinstance(n_cycles, int):
-        n_cycles = np.array(n_cycles).ravel()
-    else:
-        n_cycles = np.array([n_cycles])
     inv_fs = 1.0 / fs
     index = 0
     sigma_t = np.zeros(freqs.size)
@@ -53,34 +50,37 @@ def create_wavelets(
             sigma_t[i] = n_cycles[index] / (2.0 * np.pi * freqs[i])
         else:
             sigma_t[i] = n_cycles[index] / (2.0 * np.pi * sigma)
-        num_values = int((5.0 * sigma_t) // inv_fs)
+        num_values = int((5.0 * sigma_t[i]) // inv_fs)
         max_value = np.maximum(num_values * 2 + 1, max_value)
+        if n_cycles.size != 1:
+            index += 1
 
     # Preallocate array since wavelets will zeropadded anyways
     t = np.zeros((freqs.size, max_value))
-
+    wave = np.zeros((freqs.size, max_value), dtype=np.complex128)
     for j in prange(freqs.size):
         # Go 5 STDEVs out on each side
-        num_values = int((5.0 * sigma_t) // inv_fs)
-        for i in range(1, num_values + 1):
-            t[j, i + num_values] = t[j, i - 1 + num_values] + inv_fs
-            t[j, num_values - i] = t[j, num_values - i + 1] + inv_fs * -1.0
+        num_values = int((5.0 * sigma_t[j]) // inv_fs)
+        wave_len = num_values * 2 + 1
+        for k in range(1, num_values + 1):
+            t[j, k + num_values] = t[j, k - 1 + num_values] + inv_fs
+            t[j, num_values - k] = t[j, num_values - k + 1] + inv_fs * -1.0
 
         # Could also do
         # oscillation.imag = np.sin(2.0 * np.pi* f * t)
         # oscillation.real = np.cos(2.0 * np.pi* f * t)
 
-        t[j] = np.exp(2.0 * 1j * np.pi * freqs[j] * t[j])
+        wave[j, :wave_len] = np.exp(2.0 * 1j * np.pi * freqs[j] * t[j, :wave_len])
 
         if zero_mean:
-            real_offset = np.exp(-2 * (np.pi * freqs[j] * sigma_t) ** 2)
-            t[j] -= real_offset
-        gaussian_env = np.exp(-(t**2) / (2.0 * sigma_t**2))
-        t[j] *= gaussian_env
-        t[j] /= np.sqrt(0.5) * np.linalg.norm(t[j], ord=ord)
-        if n_cycles.size != 1:
-            index += 1
-    return Ws
+            real_offset = np.exp(-2 * (np.pi * freqs[j] * sigma_t[j]) ** 2)
+            wave[j, :wave_len] -= real_offset
+        gaussian_env = np.exp(-(t[j, :wave_len] ** 2) / (2.0 * sigma_t[j] ** 2))
+        wave[j, :wave_len] *= gaussian_env
+        wave[j, :wave_len] /= np.sqrt(0.5) * np.linalg.norm(
+            wave[j, :wave_len], ord=order
+        )
+    return wave
 
 
 def compute_cwt(
@@ -93,9 +93,15 @@ def compute_cwt(
     n_cycles: int = 7,
     sigma: int = -1,
     zero_mean: bool = True,
-    ord: Literal[1, 2] = 2,
+    order: Literal[1, 2] = 2,
     wavelets: list[np.ndarray[np.complex128]] = None,
+    skip_fft=False,
 ):
+    if not isinstance(n_cycles, int):
+        n_cycles = np.array(n_cycles).ravel()
+    else:
+        n_cycles = np.array([n_cycles])
+
     if scaling == "linear":
         freqs = np.linspace(start=f0, stop=f1, num=num)
     else:
@@ -107,19 +113,28 @@ def compute_cwt(
             n_cycles=n_cycles,
             sigma=sigma,
             zero_mean=zero_mean,
-            ord=ord,
+            order=order,
         )
     input_size = array.size
-    max_size = max(w.size for w in wavelets)
-    nfft = input_size + max_size - 1
+    nfft = input_size + wavelets.shape[1] - 1
     nfft = fft.next_fast_len(nfft)  # 2 ** int(np.ceil(np.log2(nfft)))
-
-    array_fft = fft.fft(array, n=nfft)
     output = np.zeros((num, input_size), dtype=np.complex128)
-    for index, ws in enumerate(wavelets):
-        if ws.dtype != np.complex_:
-            w_fft = fft.fft(ws, n=nfft)
-        ret = fft.ifft(w_fft * array_fft)[: input_size + ws.size - 1]
+
+    workers = multiprocessing.cpu_count()
+    # array_fft = pyfftw.interfaces.scipy_fft.fft(array, n=nfft, workers=workers)
+    array_fft = fft.fft(array, n=nfft)
+
+    for index in range(wavelets.shape[0]):
+        if not skip_fft:
+            # output_f = pyfftw.interfaces.scipy_fft.fft(
+            #     wavelets[index], n=nfft, workers=workers
+            # )
+            output_f = fft.fft(wavelets[index], n=nfft)
+        else:
+            output_f = wavelets[index]
+        # ret = pyfftw.interfaces.scipy_fft.fft(output_f * array_fft, workers=workers)
+        ret = fft.ifft(output_f * array_fft)
+        ret = ret[: input_size + wavelets.shape[1] - 1]
 
         newsize = input_size
         currsize = ret.size
