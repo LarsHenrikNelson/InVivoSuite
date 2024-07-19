4a935f29538a3efa12040bfd618b5f28e8320ec9
Lars Henrik Nelson <larshnelson@protonmail.com>
2024-07-19 13:04:45 -0400

    invivosuite.acq.spike_functions

--- invivosuite/functions/spike_functions/sttc_methods.py
+++ invivosuite/functions/spike_functions/sttc_methods.py
@@ -1,4 +1,4 @@
-from typing import Literal, Union
+from typing import Literal, Union, Optional
 
 import networkx as nx
 import numpy as np
@@ -18,21 +18,35 @@ __all__ = [
     "sttc",
 ]
 
+Iterable = Union[np.ndarray, list]
+
 
 def create_sttc_graph(
-    cluster1_ids: np.ndarray,
-    cluster2_ids: np.ndarray,
-    sttc_values: np.ndarray,
-    connections: np.ndarray,
+    cluster1_ids: Iterable,
+    cluster2_ids: Iterable,
+    sttc_values: Iterable,
+    connections: Iterable,
+    edge_args: Optional[dict[str, Iterable]] = None,
+    node_args: Optional[dict[str, Iterable]] = None,
 ):
     sttc_graph = nx.Graph(n_units=cluster1_ids.size)
-    for i in range(cluster1_ids.size):
+    for i in range(len(cluster1_ids)):
         if connections[i]:
             sttc_graph.add_edge(
                 cluster1_ids[i],
                 cluster2_ids[i],
                 weight=sttc_values[i],
             )
+            if edge_args is not None:
+                for key in edge_args.keys():
+                    sttc_graph.edges[cluster1_ids[i]][cluster2_ids[i]][key] = edge_args[
+                        key
+                    ][i]
+    if node_args is not None:
+        cluster_ids = node_args["cluster_id"]
+        for i in range(len(cluster_ids)):
+            for key, value in node_args.items():
+                sttc_graph.nodes[cluster_ids[i]][key] = value[i]
     return sttc_graph
 
 
