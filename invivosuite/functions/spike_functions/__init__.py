48c2f1efed62e6cb817455b3fc023793c6ac7b27
Lars Henrik Nelson <larshnelson@protonmail.com>
2026-03-27 16:41:35 -0400

    Fixed imports from spike functions.

--- invivosuite/functions/spike_functions/__init__.py
+++ invivosuite/functions/spike_functions/__init__.py
@@ -1,6 +1,6 @@
 from .binarize_spikes import bin_spikes, create_binary_spikes
 from .continuous_fr import *
-from .intrinsic_timescale import SExpDecay, sttc_autocorr
+from .intrinsic_timescale import SExpDecay, _sttc_positive_lags, sttc_crosscorr
 from .max_interval_bursts import get_burst_data, max_int_bursts
 from .network_functions import *
 from .spike_correlation_index import correlation_index
